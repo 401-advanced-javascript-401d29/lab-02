4af7e95952ab2623b2208429754dd50ff823095a
Erin Trainor <trainor.erin@gmail.com>
2019-03-19 23:23:23 -0700

    moving files

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 // These 2 should be interchangeable!
-const List = require('../list.js');
-// const List = require('../list-constructor.js');
+// const List = require('../list.js');
+const List = require('../list-constructor.js');
 
 describe('List Data Structure', () => {
 
-  it('starts with a length of -1 and an empty data set', () => {
+  test('starts with a length of -1 and an empty data set', () => {
     let stuff = new List();
     expect(stuff.length).toEqual(0);
     expect(stuff.data).toEqual({});
   });
 
-  it('pushes items to the end of the data set', () => {
+  test('pushes items to the end of the data set', () => {
     let stuff = new List();
     stuff.push('a');
     stuff.push('b');
@@ -20,4 +20,44 @@ describe('List Data Structure', () => {
     expect(stuff.data[1]).toEqual('b');
   });
 
+  test('shifts\\removes items from the beginning of the data set', () => {
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+    expect(stuff.shift()).toEqual('a');
+    expect(stuff.data[0]).toEqual('b');
+  });
+
+  // test('unshifts\\adds items to the beginning the data set', () => {
+  //   let stuff = new List();
+  //   stuff.push('a');
+  //   stuff.push('b');
+  //   expect(stuff.unshift('z')).toEqual('a');
+  //   expect(stuff.data[0]).toEqual('z');
+  // })
 });
+
+// NEW VERSION OF STARTER CODE... MAY NEED TO REVIEW AND REFACTOR
+// 'use strict';
+//
+// // These 2 should be interchangeable!
+// const List = require('../list.js');
+// // const List = require('../list-constructor.js');
+//
+// describe('List Data Structure', () => {
+//
+//   it('starts with a length of -1 and an empty data set', () => {
+//     let stuff = new List();
+//     expect(stuff.length).toEqual(0);
+//     expect(stuff.data).toEqual({});
+//   });
+//
+//   it('pushes items to the end of the data set', () => {
+//     let stuff = new List();
+//     stuff.push('a');
+//     stuff.push('b');
+//     expect(stuff.length).toEqual(2);
+//     expect(stuff.data[1]).toEqual('b');
+//   });
+//
+// });
